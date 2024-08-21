4759a37c35dbae5510ed709bcce37c410e3e2f6c
navyabalakrishnan <navyabkrishna176@gmail.com>
2024-08-21 11:52:09 +0530

    commitRR

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(
   cors({
     origin:
-    "http://localhost:5173", 
+    // "http://localhost:5173", 
+    "https://ecommerce-frontend-xi-eight.vercel.app/",
     credentials: true,
   })
 );
