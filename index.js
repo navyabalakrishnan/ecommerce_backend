9badcea040aa69111ee1240b0fa22f319bd1a53a
navyabalakrishnan <navyabkrishna176@gmail.com>
2024-08-28 12:53:57 +0530

    updatedindex

--- index.js
+++ index.js
@@ -5,15 +5,43 @@ import dbconnect from "./src/config/dbConfig.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 const app = express();
+// app.use(
+//   cors({
+//     origin:'*',
+//   // "https://ecommerce-frontend-sigma-seven.vercel.app",
+//   // "http://localhost:5173",
+ 
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = [
+  // "https://test-beta-sooty-98.vercel.app",
+  // "https://test-foks-three.vercel.app"
+];
 app.use(
   cors({
-    origin:'*',
-  // "https://ecommerce-frontend-sigma-seven.vercel.app",
-  // "http://localhost:5173",
- 
+    origin: (origin, callback) => {
+      // Allow requests with no origin, like mobile apps or curl requests
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "X-CSRF-Token",
+      "Access-Control-Allow-Origin"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
   })
 );
+
 app.use(cookieParser())
 
 app.use(express.json());
@@ -26,4 +54,4 @@ app.listen(serverConfig.Port, () => {
   console.log(`App listening on port ${serverConfig.Port}`)
   dbconnect();
   console.log("Database is connected")
-})
+})   
