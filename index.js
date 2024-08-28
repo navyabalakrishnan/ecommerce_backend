283db133b41a1b3905863c1eb5648ee8727544c8
navyabalakrishnan <navyabkrishna176@gmail.com>
2024-08-28 13:30:40 +0530

    updatedorigin

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const app = express();
 app.use(
   cors(
       {
-          origin: [ "http://localhost:5173"],
+          origin: [ "http://localhost:5173","https://ecommerce-felive.vercel.app"],
           methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
           allowedHeaders: ['Content-Type', 'Authorization'],
           credentials: true,
